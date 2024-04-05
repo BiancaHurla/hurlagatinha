ca7b878a0f6da3bef72151b9a6e79b855f0054d7
BiancaHurla <162705238+BiancaHurla@users.noreply.github.com>
2024-04-05 09:43:07 -0300

    .

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const botoes = document.querySelectorAll(".botao")
+const botoes = document.querySelectorAll(".botao");
+console.log(botoes)
 
-for(let i=0; i <botoes.length; i++){
-    botoes[i].onclick = function(){
-
-for(let j=0; j<botoes.length; j++){
-    botoes[j].classList.remove("ativo");
+for(let i=0; i <  botoes.length; i++){
+    console.log(i);
 }
-
-        botoes[i].classList.add("ativo")/
-    }
-    console.log
+botoes[i].onclick = function(){
+    
+    botoes[i].classList.add("ativo");
+}
+for(let j=0;j<botoes.length;j++){
+    botoes[j].classList.remove("ativo");
 }
